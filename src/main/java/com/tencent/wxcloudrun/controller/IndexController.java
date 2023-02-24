c68ce90e93ff876f1736a2af0b0b9794e35a87fa
tianle <tianle@liepin.com>
2023-02-24 11:12:57 +0800

    init

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,21 +2,32 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * index控制器
  */
 @Controller
-
+@Slf4j
 public class IndexController {
 
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "index";
-  }
+    /**
+     * 主页页面
+     *
+     * @return API response html
+     */
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/validate")
+    @ResponseBody
+    public String validate(String signature, String timestamp, String nonce, String echostr) {
+        log.info("signature={},timestamp={},nonce={},echostr={}", signature, timestamp, nonce, echostr);
+        return echostr;
+    }
 
 }
